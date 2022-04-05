3689b8caf2ab21106b358180286371097343a218
Bavon101 <bavonfate@gmail.com>
2022-04-05 13:29:50 +0300

    Add missionreducer to rooteReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, createStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { missionsReducer } from '../components/missions/redux/missions';
 
-// eslint-disable-next-line import/prefer-default-export
-export const store = configureStore({
-  reducer: {
-
-  },
+const rootReducer = combineReducers({
+  missionsReducer,
 });
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
