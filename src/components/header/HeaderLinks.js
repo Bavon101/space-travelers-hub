4f8abb4906020bbcea6d995cba8147eb026ef297
Thermiee <navyblack16@yahoo.com>
2022-04-05 10:45:59 +0100

    change  rockets url to home

--- src/components/header/HeaderLinks.js
+++ src/components/header/HeaderLinks.js
@@ -5,7 +5,7 @@ import Vr from '../Vr';
 export default function HeaderLinks() {
   return (
     <div className="links">
-      <Link to="/rockets" className="link">Rockets</Link>
+      <Link to="/" className="link">Rockets</Link>
       <Link to="/missions" className="link">Missions</Link>
       <Vr />
       <Link to="/profile" className="link">My Profile</Link>
