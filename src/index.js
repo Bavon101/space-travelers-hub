4d8235627a20ae9c18f1bd0cfcf397a1f2774254
Bavon101 <bavonfate@gmail.com>
2022-04-05 13:44:35 +0300

    Hotfix: linter errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from './app/store';
+import store from './app/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
