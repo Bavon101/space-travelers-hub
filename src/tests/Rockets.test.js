5203db4817c2ccde09dc27c44fb90456dcb4de39
Thermiee <navyblack16@yahoo.com>
2022-04-07 13:01:14 +0100

    fix linter error

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -6,35 +6,33 @@ import store from '../app/store';
 import HomePage from '../components/pages/HomePage';
 
 const RocketsProvider = () => (
-    <Provider store={store}>
-        <HomePage />
-    </Provider>
+  <Provider store={store}>
+    <HomePage />
+  </Provider>
 );
 
-
-describe ('Check Homepage', () => {
-    it('renders correctly', () => {
-       const component = renderer.create(<RocketsProvider />).toJSON();
-         expect(component).toMatchSnapshot();
-    });
+describe('Check Homepage', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(<RocketsProvider />).toJSON();
+    expect(component).toMatchSnapshot();
+  });
 });
 
-describe ('No Rocket Available', () => {
-    it('renders correctly', () => {
-        render(<RocketsProvider />);
-        expect(screen.getByText('No rockets available')).toBeInTheDocument();
-    });
+describe('No Rocket Available', () => {
+  it('renders correctly', () => {
+    render(<RocketsProvider />);
+    expect(screen.getByText('No rockets available')).toBeInTheDocument();
+  });
 });
 
 describe('Rockets Component', () => {
-    it('renders rockets', async () => {
-      render(<RocketsProvider />);
-      const allRockets = await screen.findAllByText('Reserve Rocket');
-      expect(allRockets).toHaveLength(4);
-    }); 
+  it('renders rockets', async () => {
+    render(<RocketsProvider />);
+    const allRockets = await screen.findAllByText('Reserve Rocket');
+    expect(allRockets).toHaveLength(4);
+  });
 
-
-test ('Reserve a rocket and cancel reservation', async () => {
+  test('Reserve a rocket and cancel reservation', async () => {
     render(<RocketsProvider />);
     const reserveButton = await screen.findAllByText('Reserve Rocket');
     const badges = await screen.findAllByText('Reserved');
@@ -42,5 +40,5 @@ test ('Reserve a rocket and cancel reservation', async () => {
     fireEvent.click(reserveButton[0]);
     expect(badges[0].textContent).toBe('Reserved');
     expect(reserveButton[0].textContent).toBe('Cancel Reservation');
-    });
+  });
 });
